dc57d9ce1ee838fac0fa2ec9affcc7890f4616bb
fouedhadded <foued.hadded@jadenx.com>
2022-10-16 15:58:30 +0200

    Set up interaction with SC interface

---
+++ weekendproj/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
